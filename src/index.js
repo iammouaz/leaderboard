0c7f8fc1886ff3f470c9c39bab04f8c97326abbc
iammouaz <88675267+iammouaz@users.noreply.github.com>
2021-10-14 02:01:52 +0300

    Add Submit method

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ const gameID = 'Zl4d7IVkemOTTVg2fUd2';
 const URL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores`;
 const Refresh = document.getElementById('refrsh');
 const scoreList = document.getElementById('scores');
+const AddName = document.getElementById('name');
+const AddScore = document.getElementById('score');
+const PostToAPI = document.getElementById('submit');
+
 const transformData = (data) => {
   data.forEach((result) => {
     const { user, score } = result;
@@ -34,9 +38,33 @@ const GetScoreGame = async () => {
   }
 };
 
+const PostRequst = async () => {
+  if (AddName.value && AddScore.value) {
+    try {
+      await fetch(URL, {
+        method: 'POST',
+        body: JSON.stringify({
+          user: AddName.value,
+          score: AddScore.value,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      AddScore.value = '';
+      AddName.value = '';
+      window.location.reload();
+    } catch (error) {
+      throw new Error('Please check POST API Mehtod');
+    }
+  }
+};
+
 document.addEventListener('DOMContentLoaded', () => {
   GetScoreGame();
 });
+
+PostToAPI.addEventListener('click', PostRequst);
 
 Refresh.addEventListener('click', () => {
   window.location.reload();
